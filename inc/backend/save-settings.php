7035930de63ba0db541083b93fb12fcdcae969a2
Access Keys <Access Keys@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-18 10:50:48 +0000

    * Done the div fixing for the settings page. * Compactibility test for wordpress v4.2
    git-svn-id: https://plugins.svn.wordpress.org/accesspress-custom-css/tags/1.0.3@1183316 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inc/backend/save-settings.php
@@ -0,0 +1,12 @@
+<?php
+defined('ABSPATH') or die( 'No script kiddies please!' );
+$apccss_settings = array();
+$apccss_textarea = $_POST['apccss_textarea'];
+if ( $_POST['action'] == 'apccss_save_options' ) {
+	$apccss_settings['css'] = $apccss_textarea;
+	update_option( APCCSS_SETTINGS, $apccss_settings);
+	$_SESSION['apccss_message'] = __( 'Custom CSS Saved Successfully.', APCCSS_TEXT_DOMAIN );
+    wp_redirect( admin_url() . 'admin.php?page='.APCCSS_TEXT_DOMAIN );
+    exit;
+}
+ ?>
